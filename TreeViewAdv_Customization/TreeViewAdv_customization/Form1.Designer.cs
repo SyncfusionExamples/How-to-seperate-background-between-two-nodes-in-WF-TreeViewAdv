19ee545b293eff0561fa7477c70e4fb3d10fba0b
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2020-12-09 16:53:46 +0530

    Modified the sample code

--- TreeViewAdv_Customization/TreeViewAdv_customization/Form1.Designer.cs
+++ TreeViewAdv_Customization/TreeViewAdv_customization/Form1.Designer.cs
@@ -28,147 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Syncfusion.Windows.Forms.Tools.TreeNodeAdvStyleInfo treeNodeAdvStyleInfo1 = new Syncfusion.Windows.Forms.Tools.TreeNodeAdvStyleInfo();
-            Syncfusion.Windows.Forms.Tools.TreeNodeAdv treeNodeAdv1 = new Syncfusion.Windows.Forms.Tools.TreeNodeAdv();
-            Syncfusion.Windows.Forms.Tools.TreeNodeAdv treeNodeAdv2 = new Syncfusion.Windows.Forms.Tools.TreeNodeAdv();
-            Syncfusion.Windows.Forms.Tools.TreeNodeAdv treeNodeAdv3 = new Syncfusion.Windows.Forms.Tools.TreeNodeAdv();
-            Syncfusion.Windows.Forms.Tools.TreeNodeAdv treeNodeAdv4 = new Syncfusion.Windows.Forms.Tools.TreeNodeAdv();
-            Syncfusion.Windows.Forms.Tools.TreeNodeAdv treeNodeAdv5 = new Syncfusion.Windows.Forms.Tools.TreeNodeAdv();
-            Syncfusion.Windows.Forms.Tools.TreeNodeAdv treeNodeAdv6 = new Syncfusion.Windows.Forms.Tools.TreeNodeAdv();
-            this.treeViewAdv1 = new Syncfusion.Windows.Forms.Tools.TreeViewAdv();
-            ((System.ComponentModel.ISupportInitialize)(this.treeViewAdv1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // treeViewAdv1
-            // 
-            this.treeViewAdv1.BackgroundColor = new Syncfusion.Drawing.BrushInfo(System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255))))));
-            treeNodeAdvStyleInfo1.CheckBoxTickThickness = 1;
-            treeNodeAdvStyleInfo1.CheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdvStyleInfo1.EnsureDefaultOptionedChild = true;
-            treeNodeAdvStyleInfo1.IntermediateCheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdvStyleInfo1.OptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdvStyleInfo1.SelectedOptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
-            treeNodeAdvStyleInfo1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(43)))), ((int)(((byte)(43)))));
-            this.treeViewAdv1.BaseStylePairs.AddRange(new Syncfusion.Windows.Forms.Tools.StyleNamePair[] {
-            new Syncfusion.Windows.Forms.Tools.StyleNamePair("Standard", treeNodeAdvStyleInfo1)});
-            this.treeViewAdv1.BeforeTouchSize = new System.Drawing.Size(209, 241);
-            this.treeViewAdv1.Border3DStyle = System.Windows.Forms.Border3DStyle.Flat;
-            this.treeViewAdv1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.treeViewAdv1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(43)))), ((int)(((byte)(43)))));
-            // 
-            // 
-            // 
-            this.treeViewAdv1.HelpTextControl.BaseThemeName = null;
-            this.treeViewAdv1.HelpTextControl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.treeViewAdv1.HelpTextControl.Location = new System.Drawing.Point(0, 0);
-            this.treeViewAdv1.HelpTextControl.Name = "";
-            this.treeViewAdv1.HelpTextControl.Size = new System.Drawing.Size(15, 15);
-            this.treeViewAdv1.HelpTextControl.TabIndex = 0;
-            this.treeViewAdv1.HelpTextControl.Visible = true;
-            this.treeViewAdv1.InactiveSelectedNodeForeColor = System.Drawing.SystemColors.ControlText;
-            this.treeViewAdv1.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(94)))), ((int)(((byte)(94)))));
-            this.treeViewAdv1.Location = new System.Drawing.Point(159, 39);
-            this.treeViewAdv1.Margin = new System.Windows.Forms.Padding(2);
-            this.treeViewAdv1.MetroColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(165)))), ((int)(((byte)(220)))));
-            this.treeViewAdv1.Name = "treeViewAdv1";
-            treeNodeAdv1.ChildStyle.CheckBoxTickThickness = 1;
-            treeNodeAdv1.ChildStyle.CheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv1.ChildStyle.EnsureDefaultOptionedChild = true;
-            treeNodeAdv1.ChildStyle.IntermediateCheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv1.ChildStyle.OptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv1.ChildStyle.SelectedOptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
-            treeNodeAdv1.EnsureDefaultOptionedChild = true;
-            treeNodeAdv1.Expanded = true;
-            treeNodeAdv1.MultiLine = true;
-            treeNodeAdv2.ChildStyle.CheckBoxTickThickness = 1;
-            treeNodeAdv2.ChildStyle.CheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv2.ChildStyle.EnsureDefaultOptionedChild = true;
-            treeNodeAdv2.ChildStyle.IntermediateCheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv2.ChildStyle.OptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv2.ChildStyle.SelectedOptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
-            treeNodeAdv2.EnsureDefaultOptionedChild = true;
-            treeNodeAdv2.MultiLine = true;
-            treeNodeAdv2.Optioned = true;
-            treeNodeAdv2.ShowLine = true;
-            treeNodeAdv2.Text = "India";
-            treeNodeAdv3.ChildStyle.CheckBoxTickThickness = 1;
-            treeNodeAdv3.ChildStyle.CheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv3.ChildStyle.EnsureDefaultOptionedChild = true;
-            treeNodeAdv3.ChildStyle.IntermediateCheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv3.ChildStyle.OptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv3.ChildStyle.SelectedOptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
-            treeNodeAdv3.EnsureDefaultOptionedChild = true;
-            treeNodeAdv3.MultiLine = true;
-            treeNodeAdv3.ShowLine = true;
-            treeNodeAdv3.Text = "China";
-            treeNodeAdv1.Nodes.AddRange(new Syncfusion.Windows.Forms.Tools.TreeNodeAdv[] {
-            treeNodeAdv2,
-            treeNodeAdv3});
-            treeNodeAdv1.ShowLine = true;
-            treeNodeAdv1.Text = "Asia";
-            treeNodeAdv4.ChildStyle.CheckBoxTickThickness = 1;
-            treeNodeAdv4.ChildStyle.CheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv4.ChildStyle.EnsureDefaultOptionedChild = true;
-            treeNodeAdv4.ChildStyle.IntermediateCheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv4.ChildStyle.OptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv4.ChildStyle.SelectedOptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
-            treeNodeAdv4.EnsureDefaultOptionedChild = true;
-            treeNodeAdv4.Expanded = true;
-            treeNodeAdv4.MultiLine = true;
-            treeNodeAdv5.ChildStyle.CheckBoxTickThickness = 1;
-            treeNodeAdv5.ChildStyle.CheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv5.ChildStyle.EnsureDefaultOptionedChild = true;
-            treeNodeAdv5.ChildStyle.IntermediateCheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv5.ChildStyle.OptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv5.ChildStyle.SelectedOptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
-            treeNodeAdv5.EnsureDefaultOptionedChild = true;
-            treeNodeAdv5.Expanded = true;
-            treeNodeAdv5.MultiLine = true;
-            treeNodeAdv6.ChildStyle.CheckBoxTickThickness = 1;
-            treeNodeAdv6.ChildStyle.CheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv6.ChildStyle.EnsureDefaultOptionedChild = true;
-            treeNodeAdv6.ChildStyle.IntermediateCheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv6.ChildStyle.OptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            treeNodeAdv6.ChildStyle.SelectedOptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
-            treeNodeAdv6.EnsureDefaultOptionedChild = true;
-            treeNodeAdv6.MultiLine = true;
-            treeNodeAdv6.Optioned = true;
-            treeNodeAdv6.ShowLine = true;
-            treeNodeAdv6.Text = "Washington";
-            treeNodeAdv5.Nodes.AddRange(new Syncfusion.Windows.Forms.Tools.TreeNodeAdv[] {
-            treeNodeAdv6});
-            treeNodeAdv5.Optioned = true;
-            treeNodeAdv5.ShowLine = true;
-            treeNodeAdv5.Text = "United States";
-            treeNodeAdv4.Nodes.AddRange(new Syncfusion.Windows.Forms.Tools.TreeNodeAdv[] {
-            treeNodeAdv5});
-            treeNodeAdv4.ShowLine = true;
-            treeNodeAdv4.Text = "North America";
-            this.treeViewAdv1.Nodes.AddRange(new Syncfusion.Windows.Forms.Tools.TreeNodeAdv[] {
-            treeNodeAdv1,
-            treeNodeAdv4});
-            this.treeViewAdv1.SelectedNodeBackground = new Syncfusion.Drawing.BrushInfo(System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(197)))), ((int)(((byte)(197))))));
-            this.treeViewAdv1.SelectedNodeForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(43)))), ((int)(((byte)(43)))));
-            this.treeViewAdv1.ShowFocusRect = false;
-            this.treeViewAdv1.Size = new System.Drawing.Size(209, 241);
-            this.treeViewAdv1.Style = Syncfusion.Windows.Forms.Tools.TreeStyle.Office2016Colorful;
-            this.treeViewAdv1.TabIndex = 0;
-            this.treeViewAdv1.Text = "treeViewAdv1";
-            this.treeViewAdv1.ThemeName = "Office2016Colorful";
-            this.treeViewAdv1.ThemeStyle.TreeNodeAdvStyle.CheckBoxTickThickness = 0;
-            this.treeViewAdv1.ThemeStyle.TreeNodeAdvStyle.EnsureDefaultOptionedChild = true;
-            // 
-            // 
-            // 
-            this.treeViewAdv1.ToolTipControl.BackColor = System.Drawing.SystemColors.Info;
-            this.treeViewAdv1.ToolTipControl.BaseThemeName = null;
-            this.treeViewAdv1.ToolTipControl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.treeViewAdv1.ToolTipControl.Location = new System.Drawing.Point(0, 0);
-            this.treeViewAdv1.ToolTipControl.Name = "";
-            this.treeViewAdv1.ToolTipControl.Size = new System.Drawing.Size(15, 15);
-            this.treeViewAdv1.ToolTipControl.TabIndex = 0;
-            this.treeViewAdv1.ToolTipControl.Visible = true;
-            this.treeViewAdv1.TransparentControls = true;
             // 
             // Form1
             // 
@@ -177,18 +37,14 @@
             this.CaptionBarColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(158)))), ((int)(((byte)(218)))));
             this.CaptionButtonColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(535, 294);
-            this.Controls.Add(this.treeViewAdv1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "TreeViewAdv_customization";
-            ((System.ComponentModel.ISupportInitialize)(this.treeViewAdv1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Syncfusion.Windows.Forms.Tools.TreeViewAdv treeViewAdv1;
     }
 }
 
--- TreeViewAdv_Customization/TreeViewAdv_customization/Form1.cs
+++ TreeViewAdv_Customization/TreeViewAdv_customization/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using Syncfusion.Windows.Forms;
+using Syncfusion.Windows.Forms.Tools;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -17,13 +18,88 @@ namespace TreeViewAdv_customization
         public Form1()
         {
             InitializeComponent();
-            this.treeViewAdv1.OwnerDrawNodes = true;
+
+            TreeViewAdv treeViewAdv1 = new TreeViewAdv();            
+            treeViewAdv1.Name = "treeView1";
+            treeViewAdv1.Size = new Size(209, 241);
+            treeViewAdv1.Location = new Point(159, 39);
+            treeViewAdv1.Style = TreeStyle.Office2016Colorful;
+
+            //Create instance of TreeNodeAdv named treeNode1
+            TreeNodeAdv treeNode1 = new TreeNodeAdv("Node1");
+            TreeNodeAdv treeNode7 = new TreeNodeAdv("Node7");
+
+            //Adding the subnode of [treeNode1] in Root [treeNode2].
+            TreeNodeAdv treeNode2 = new TreeNodeAdv("Node0", new TreeNodeAdv[] {
+            treeNode1, treeNode7});
+            treeNode2.Expanded = true;
+
+            //Create instance of TreeNodeAdv named treeNode3
+            TreeNodeAdv treeNode3 = new TreeNodeAdv("Node3");
+            TreeNodeAdv treeNode8 = new TreeNodeAdv("Node8");
+
+            //Adding the subnode of [treeNode3 ] in Root [treeNode4].
+            TreeNodeAdv treeNode4 = new TreeNodeAdv("Node2", new TreeNodeAdv[] {
+            treeNode3, treeNode8});
+            treeNode4.Expanded = true;
+
+            // Create instance of TreeNodeAdv named treeNode5
+            TreeNodeAdv treeNode5 = new TreeNodeAdv("Node5");
+            TreeNodeAdv treeNode9 = new TreeNodeAdv("Node9");
+
+            //Adding the subnode of [treeNode5] in Root [treeNode6].
+            TreeNodeAdv treeNode6 = new TreeNodeAdv("Node4", new TreeNodeAdv[] {
+            treeNode5, treeNode9});
+            treeNode6.Expanded = true;
+
+            //Assigning the text and Name
+
+            treeNode1.Name = "Node1";
+            treeNode1.Text = "India";
+
+            treeNode7.Name = "Node7";
+            treeNode7.Text = "China";
+
+            treeNode2.Name = "Node0";
+            treeNode2.Text = "Asia";
+
+            treeNode3.Name = "Node3";
+            treeNode3.Text = "Britain";
+
+            treeNode8.Name = "Node8";
+            treeNode8.Text = "France";
+
+            treeNode4.Name = "Node2";
+            treeNode4.Text = "Europe";
+
+            treeNode5.Name = "Node5";
+            treeNode5.Text = "South Africa";
+
+            treeNode9.Name = "Node9";
+            treeNode9.Text = "Zimbabwe";
+
+            treeNode6.Name = "Node4";
+            treeNode6.Text = "Africa";
+
+            //Add the nodes in TreeViewAdv nodes collection
+
+            treeViewAdv1.Nodes.AddRange(new TreeNodeAdv[] {
+            treeNode2,
+            treeNode4,
+            treeNode6});
+
+
+            treeViewAdv1.OwnerDrawNodes = true;
             this.StartPosition = FormStartPosition.CenterScreen;
-            this.treeViewAdv1.BeforeNodePaint += TreeViewAdv1_BeforeNodePaint1;
+            treeViewAdv1.BeforeNodePaint += TreeViewAdv1_BeforeNodePaint1;
+
+            // Add the TreeViewAdv to the Form controls
+            this.Controls.Add(treeViewAdv1);
+
         }
 
         //Customize Background of Nodes
-        private void TreeViewAdv1_BeforeNodePaint1(object sender, Syncfusion.Windows.Forms.Tools.TreeNodeAdvPaintEventArgs e)
+        private void TreeViewAdv1_BeforeNodePaint1(object sender, TreeNodeAdvPaintEventArgs e)
         {
             LinearGradientBrush lBrush = new LinearGradientBrush(e.Node.Bounds, Color.LightGreen, Color.LightSeaGreen, LinearGradientMode.Horizontal);
             e.Graphics.FillRectangle(lBrush, e.Node.Bounds);
